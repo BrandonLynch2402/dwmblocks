96e300c344756143c14e457e969c5d2a27cbcf18
Brandon Lynch <brandon.lynch2402@gmail.com>
2020-12-26 11:35:10 -0600

    Init

---
+++ blocks.def.h
@@ -0,0 +1,13 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{" ", "xmas_countdown", 0, 0},
+	{"", "wttr", 1200, 0},
+	{"", "volume", 0, 10},
+	{"", "disk", 300, 0},
+	{"📅 ", "date '+%a %b %d, %Y %I:%M %p'", 5, 0},
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim[] = " | ";
+static unsigned int delimLen = 5;
