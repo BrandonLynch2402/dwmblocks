2b92ab64b869e244007320dc9b78fabed2744374
Brandon Lynch <lynchb@objectcomputing.com>
2022-09-01 22:44:14 -0500

    Added spooky countdown

--- blocks.def.h
+++ blocks.def.h
@@ -1,7 +1,7 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{" ", "get_stock_price.py", 3600, 0},
+	{" ", "spooky_countdown", 3600, 0},
 	{"", "bday_countdown", 3600, 0},
 	{"", "check_updates", 3600, 20},
 	{"", "wttr", 1200, 0},
